3e8df4b8d87b27e36d6c9b2b14778d476b7b93ce
ashoordoushow <ashoor12323@gmail.com>
2024-10-22 20:13:29 -0500

    random frontend api

---
+++ random-api-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { PicturesPage } from "./PicturesPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PicturesPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
